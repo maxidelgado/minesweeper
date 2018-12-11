ae46bcff0a08eb382de0330e474f07871f52d9cb
Maximiliano Delgado <maxi.delga2@gmail.com>
2018-12-11 19:51:20 -0300

    CORS added and corrected error in swagger's object using

--- routers/router.go
+++ routers/router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"github.com/minesweeper/controllers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
 )
 
 // @APIVersion 1.0.0
@@ -10,6 +11,11 @@ import (
 // @Description Web Minesweeper challenge for Fukuroo!
 // @Contact maxi.delga2@gmail.com
 func init() {
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "POST", "GET"},
+	}))
+
 	ns := beego.NewNamespace("/api/v1",
 		beego.NSNamespace("/games",
 			beego.NSInclude(
